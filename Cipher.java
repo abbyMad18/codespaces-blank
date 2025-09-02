e244525940e5aa9543d54ea9ac7851d7cb7b8bc3
abbyMad18 <abbymbruskin@gmail.com>
2025-09-02 16:43:36 +0000

    first commit

---
+++ Cipher.java
@@ -0,0 +1,45 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Cipher {
+
+    private static List<Character> alphabet = List.of('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+
+    public static String decode(String word) {
+        String result = "";
+        word = word.toLowerCase();
+        for (char letter: word.toCharArray()) {
+            if (!(letter==(' ')))
+            {
+                int index = alphabet.indexOf(letter);
+                if (index == 24)
+                {
+                    result += alphabet.get(0);
+                }
+                else if (index == 25)
+                {
+                    result += alphabet.get(1);
+                }
+                else { 
+                result += alphabet.get(index + 2);
+            }
+            }
+            else
+            {
+                result += ' ';
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+    
+   String phrase = "fcjjm rfgq gq kw rcqr";//should output hello this is my test
+   System.out.print(decode(phrase));
+
+}
+
+}
+
+
