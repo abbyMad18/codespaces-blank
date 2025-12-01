c2ca3495a2ec4882337b103d2fc2a3d54cc54ff5
abbyMad18 <abbymbruskin@gmail.com>
2025-12-01 11:02:40 -0500

    h

--- binarySearch.java
+++ binarySearch.java
@@ -20,6 +20,22 @@
 
     public static <T extends Comparable<T>>  
 
-        boolean binarySearch(T[] data, int min, int max, T target)
+        boolean binarySearch(T[] data, int min, int max, T target){
+            //base case
+            if(min == target || max == target){
+                System.out.println("found");
+                return true;
+            }
+            //greater than half
+            int half = (data.length-1)/2;
+            if(target > data[half]){
+                min = half;
+                binarySearch(T[] data, half, max, target);
+            }
+            //less than half
+            if(target < data[half]){
+                min = half;
+                binarySearch(T[] data, min, half, target);
+            }
 
     {  
